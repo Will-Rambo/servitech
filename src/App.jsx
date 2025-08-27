515dc511e65eb2f84cc562d93b86ae3741332277
Will-Rambo <willian.rambo@gmail.com>
2025-08-27 12:57:18 -0300

    Atualiza vite.config.js e ajustes para deploy

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div>
       {/* Header */}
       <header>
-        <img src="./images/logo-bordo-500-sf.png" alt="ServTech Instalações" class="logo"></img>
+        <img src="./images/logo-bordo-500-sf.png" alt="ServTech Instalações" className="logo"></img>
         {/*<h1>SERVTECH</h1>*/}
         <img src="./images/servtech-2.png" alt="ServTech Instalações" class="logo"></img>
 
